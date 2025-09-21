b4733f1b097c27707049682aa4acd4ae99063480
Vaibhav Tyagi <vaibhavtyagi828@gmail.com>
2025-09-21 13:59:01 +0530

    reupdate

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import UserClass from "./UserClass";
 class About extends React.Component{
     render(){
         return(
             <div>
                 <h1>About us Page</h1>
-                <UserClass/>
+                {/* <UserClass/> */}
             </div>
         )
     }
